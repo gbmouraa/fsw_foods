bca49f9fd6bae61ffa0f6f38440c515e40827db7
Gabriel Moura <gabrielmoura-cma@hotmail.com>
2024-05-01 10:07:09 -0300

    feat: add promo banner to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import Header from "./_components/header";
 import Search from "./_components/search";
+import CategoryList from "./_components/category-list";
+import Image from "next/image";
 
 const Home = () => {
   return (
@@ -7,6 +9,22 @@ const Home = () => {
       <Header />
       <div className="px-5 pt-6">
         <Search />
+      </div>
+
+      <div className="px-5 pt-6">
+        <CategoryList />
+      </div>
+
+      <div className="px-5 pt-6">
+        <Image
+          src="/promo-banner-01.png"
+          alt="Até 30% de desconto em pizzas"
+          height={0}
+          width={0}
+          className="h-auto w-full object-contain"
+          sizes="100vw"
+          quality={100}
+        />
       </div>
     </>
   );
